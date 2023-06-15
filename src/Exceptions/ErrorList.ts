08794bcf5fa60a54a1d1b13647b84b506df260c8
Syihabudin Tsani <syihabudintsani15@gmail.com>
2023-06-15 18:53:47 +0700

    [improve] Improve Errorlist add more Error Class

--- src/Exceptions/ErrorList.ts
+++ src/Exceptions/ErrorList.ts
@@ -20,3 +20,23 @@ export class UnAuthorize extends CustomError{
         this.message = message;
     }
 }
+
+export class Forbiden extends CustomError{
+    code: number;
+    message: string;
+    constructor(message: string){
+        super(EHttpCode.FORBIDEN, message);
+        this.code = EHttpCode.FORBIDEN;
+        this.message = message;
+    }
+}
+
+export class NotFound extends CustomError{
+    code: number;
+    message: string;
+    constructor(message: string){
+        super(EHttpCode.NOT_FOUND, message);
+        this.code = EHttpCode.NOT_FOUND;
+        this.message = message;
+    }
+}
