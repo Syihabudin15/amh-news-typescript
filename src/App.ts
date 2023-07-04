8e7bc90938e0f3be61e3fdefd42ffb1f322d82c1
Syihabudin Tsani <syihabudintsani15@gmail.com>
2023-07-05 00:27:00 +0700

    [improve] give token permision to headers

--- src/App.ts
+++ src/App.ts
@@ -30,7 +30,7 @@ class App{
         this._app.use(function (req, res, next) {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, token');
             next();
         });
         this._app.use(Express.json());
